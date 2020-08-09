9d359a95457a3ec2e7328f980633135d2d6edcd5
jarzombeg <jarzombeg@gmail.com>
2020-08-09 15:26:11 +0100

    .

--- app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -1,4 +1,13 @@
 package com.londonappbrewery.quizzler;
 
 public class TrueFalse {
+
+    private int mQuestionID;
+    private boolean mAnswer;
+
+    public TrueFalse(int questionResourceID, boolean trueOrFalse) {
+        mQuestionID = questionResourceID;
+        mAnswer = trueOrFalse;
+
+    }
 }
